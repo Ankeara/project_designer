d933c9400ea52f63efd911d5e3f6f189ce947160
DESKTOP-JQ1EBN4\Asus <ankeara017@gmail.com>
2024-10-29 11:56:36 +0700

    change new

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -116,12 +116,12 @@ const closeAlert = () => {
                 <span className="dot"></span>
                 <p className="title">Project Designer</p>
               </div>
-              <Link to={"/view"} className="btn__hire">
+              <a href="https://my-templatedesign.vercel.app/" target="blank" className="btn__hire">
                   View All <MoveRight size={16} absoluteStrokeWidth />
-              </Link>
+              </a>
             </div>
             
-            <Link to={"/view"} className="box__item">
+            <a href="https://my-templatedesign.vercel.app/mobile" target="blank"  className="box__item">
               <div className="box__detail">
                 <div className="box__img">
                   <img src={Project1} alt="me" />
@@ -132,15 +132,15 @@ const closeAlert = () => {
                   <p className="description">UI/UX Mobile App & Web Appplication</p>
                 </div>
               </div>
-              <Link to={"/view"} className="arrow">
+              <Link  className="arrow">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color="#cccc" fill="none">
                     <path d="M12.9997 11L5.99969 18" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M15.5407 6.08278L14.2989 6.19567C12.287 6.37857 11.2811 6.47002 11.0444 7.12388C10.8076 7.77774 11.5219 8.49198 12.9504 9.92046L14.0792 11.0493C15.5077 12.4778 16.222 13.1921 16.8758 12.9553C17.5297 12.7186 17.6211 11.7127 17.804 9.70078L17.9169 8.45902C18.027 7.24766 18.0821 6.64198 17.7199 6.27979C17.3577 5.9176 16.752 5.97266 15.5407 6.08278Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </Link>
-            </Link>
+            </a>
 
-            <Link to={"/view"} className="box__item">
+            <a href="https://my-templatedesign.vercel.app/website" target="blank"  className="box__item">
               <div className="box__detail">
                 <div className="box__img">
                   <img src={Project3} alt="me" />
@@ -151,15 +151,15 @@ const closeAlert = () => {
                   <p className="description">Code Mobile App & Web Appplication</p>
                 </div>
               </div>
-              <Link to={"/view"} className="arrow">
+              <Link  className="arrow">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color="#cccc" fill="none">
                     <path d="M12.9997 11L5.99969 18" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M15.5407 6.08278L14.2989 6.19567C12.287 6.37857 11.2811 6.47002 11.0444 7.12388C10.8076 7.77774 11.5219 8.49198 12.9504 9.92046L14.0792 11.0493C15.5077 12.4778 16.222 13.1921 16.8758 12.9553C17.5297 12.7186 17.6211 11.7127 17.804 9.70078L17.9169 8.45902C18.027 7.24766 18.0821 6.64198 17.7199 6.27979C17.3577 5.9176 16.752 5.97266 15.5407 6.08278Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </Link>
-            </Link>
+            </a>
 
-            <Link to={"/view"} className="box__item">
+            <a href="https://my-templatedesign.vercel.app/database" target="blank" className="box__item">
               <div className="box__detail">
                 <div className="box__img">
                   <img src={Project2} alt="me" />
@@ -170,13 +170,13 @@ const closeAlert = () => {
                   <p className="description">Code Mobile/Web Application Store Data</p>
                 </div>
               </div>
-              <Link to={"/view"} className="arrow">
+              <Link  className="arrow">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color="#cccc" fill="none">
                     <path d="M12.9997 11L5.99969 18" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                     <path d="M15.5407 6.08278L14.2989 6.19567C12.287 6.37857 11.2811 6.47002 11.0444 7.12388C10.8076 7.77774 11.5219 8.49198 12.9504 9.92046L14.0792 11.0493C15.5077 12.4778 16.222 13.1921 16.8758 12.9553C17.5297 12.7186 17.6211 11.7127 17.804 9.70078L17.9169 8.45902C18.027 7.24766 18.0821 6.64198 17.7199 6.27979C17.3577 5.9176 16.752 5.97266 15.5407 6.08278Z" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
               </Link>
-            </Link>
+            </a>
           </div>        
           
           <div className="follow__work">
